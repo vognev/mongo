069e2adf463b60d62fb52a636ad24735689b0b46
Vitaliy Ognev <vitaliy.ognev@gmail.com>
2014-12-01 20:42:41 +0200

    added prolongation

--- src/Mongo/Session/Handler.php
+++ src/Mongo/Session/Handler.php
@@ -6,6 +6,13 @@ use Mongo\Session\Model as Session;
 
 class Handler implements \SessionHandlerInterface
 {
+    protected $renewRememberedSessions = false;
+
+    public function __construct($renewRememberedSessions)
+    {
+        $this->renewRememberedSessions = !!$renewRememberedSessions;
+    }
+
     public function open($savePath, $sessionName)
     {
         try {
@@ -58,7 +65,7 @@ class Handler implements \SessionHandlerInterface
      */
     public function write($session_id, $data)
     {
-        $now            = time();
+        $invoked        = time();
         $lifetime       = (int) ini_get('session.cookie_lifetime');
 
         /** @var Session $session */
@@ -67,13 +74,13 @@ class Handler implements \SessionHandlerInterface
             $session = Session::create(array(
                 '_id'       => $session_id,
                 'lifetime'  => $lifetime,
-                'created'   => $now,
-                'updated'   => $now,
-                'expires'   => $now + $lifetime,
+                'created'   => $invoked,
+                'updated'   => $invoked,
+                'expires'   => $invoked + $lifetime,
                 'data'      => $data
             ));
         } else {
-            $session->updated   = $now;
+            $session->updated   = $invoked;
             $session->data      = $data;
         }
 
@@ -85,7 +92,7 @@ class Handler implements \SessionHandlerInterface
     {
         /** @var Session $session */
         $session = Session::load($session_id);
-
+        $invoked = time();
         // the order of invocation of sessionHandler is
         // ::open => ::read => ::gc
         // so we should check opened session for expiration
@@ -94,6 +101,41 @@ class Handler implements \SessionHandlerInterface
         if (null === $session || $session->isExpired())
             return '';
 
+        if ($this->renewRememberedSessions)
+        if ($session->lifetime > 0)
+            $this->_prolongate($session, $invoked);
+
         return $session->data;
+    }
+
+    protected function _prolongate(Session $session, $now) {
+
+        if (headers_sent()) {
+            trigger_error(__METHOD__ . ' :: headers were already sent');
+            return false;
+        }
+
+        $coll = Session::collection();
+
+        $coll->update(array(
+            '_id' => $session->_id
+        ), array(
+            '$set' => array(
+                'expires' => ($session->expires = $now + $session->lifetime),
+                'updated' => ($session->updated = $now)
+            )
+        ));
+
+        setcookie(
+            ini_get('session.name'),
+            $session->_id,
+            $session->expires,
+            ini_get('session.cookie_path'),
+            ini_get('session.cookie_domain'),
+            ini_get('session.cookie_secure'),
+            ini_get('session.cookie_httponly')
+        );
+
+        return true;
     }
 }
